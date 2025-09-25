00f4ac18e0273a9b1f1b8f7535234ab8671854d6
Aditya Narke <adityanarke10@gmail.com>
2025-09-25 23:00:36 +0530

    add.asm

--- add.asm
+++ add.asm
@@ -1,16 +1,12 @@
-section .data
-    num1 db 0x1A
-    num2 db 0x2B
-    result db 0
-
-section .text
-    global _start
-
+global _start
 _start:
-    mov al, [num1]
-    mov bl, [num2]
-    add al, bl
-    mov [result], al
-    mov eax, 1
-    xor ebx, ebx
-    int 0x80
+
+  section .text
+  mov al,100
+  mov bl,100
+  
+  add al,bl
+  
+  mov eax,1
+  xor ebx,ebx
+  int 0x80
